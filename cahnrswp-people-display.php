b60996194674e816cb50ae5d19e893b23d6cb39e
Phil Cable <pcable@wsu.edu>
2015-09-14 13:52:29 -0700

    Plugin files

---
+++ cahnrswp-people-display.php
@@ -0,0 +1,134 @@
+<?php
+/*
+Plugin Name: CAHNRSWP People Display
+Description: A plugin for displaying lists of profiles from people.wsu.edu.
+Author: CAHNRS, philcable
+Version: 0.0.1
+*/
+
+class CAHNRSWP_People_Display {
+
+	/**
+	 * Hooks.
+	 */
+	public function __construct() {
+		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ), 21 );
+		add_filter( 'wsuwp_people_html', array( $this, 'people_wrapper_html' ), 10, 3 );
+		add_filter( 'wsuwp_people_sort_items', array( $this, 'people_sort' ), 10, 1 );
+		add_filter( 'wsuwp_people_item_html', array( $this, 'people_html' ), 10, 3 );
+		add_action( 'wp_ajax_nopriv_profile_request', array( $this, 'profile_request' ) );
+		add_action( 'wp_ajax_profile_request', array( $this, 'profile_request' ) );
+	}
+
+	/**
+	 * Enqueue custom scripts.
+	 */
+	public function enqueue_scripts() {
+		$post = get_post();
+		if ( isset( $post->post_content ) && has_shortcode( $post->post_content, 'wsuwp_people' ) ) {
+			wp_enqueue_style( 'cahnrs-people', plugins_url( '/css/people.css', __FILE__ ) );
+			wp_enqueue_script( 'cahnrs-people', plugins_url( '/js/people.js', __FILE__ ), array( 'jquery' ) );
+			wp_localize_script( 'cahnrs-people', 'personnel', array( 'ajaxurl' => admin_url( 'admin-ajax.php' ) ) );
+		}
+	}
+
+	/**
+	 * The non-profile stuff.
+	 */
+	public function people_wrapper_html( $html, $people, $atts ) {
+		if ( $atts['filters'] ) {
+			$filters = explode( ',', $atts['filters'] );
+		}
+		ob_start();
+		include_once( __DIR__ . '/templates/people-actions.php' );
+		$html = ob_get_contents();
+		ob_end_clean();
+		return $html;
+	}
+
+	/**
+	 * Use the provided Content Syndicate filter to sort people results before displaying.
+	 */
+	public function people_sort( $people ) {
+		usort( $people, array( $this, 'sort_alpha' ) );
+		if ( 1 === ( count( $people ) % 2 ) ) {
+			$person = new stdClass();
+			$person->title = '';
+			$person->office = '';
+			$person->position_title = '';
+			$person->email = '';
+			$person->phone = '';
+			$people[] = $person;
+		}
+		return $people;
+	}
+	/**
+	 * Sort people alphabetically by their last name.
+	 *
+	 * @param stdClass $a Object representing a person.
+	 * @param stdClass $b Object representing a person.
+	 *
+	 * @return int Whether person a's last name is alphabetically smaller or greater than person b's.
+	 */
+	public function sort_alpha( $a, $b ) {
+		return strcasecmp( $a->last_name, $b->last_name );
+	}
+
+	/**
+	 * Provide a custom HTML template for use with syndicated people.
+	 *
+	 * @param string   $html   The HTML to output for an individual person.
+	 * @param stdClass $person Object representing a person received from people.wsu.edu.
+	 *
+	 * @return string The HTML to output for a person.
+	 */
+	public function people_html( $html, $person, $type ) {
+		if ( isset( $person->working_titles[0] ) ) {
+			$title = $person->working_titles[0];
+		} else {
+			$title = ucwords( strtolower( $person->position_title ) );
+		}
+		if ( ! empty( $person->email_alt ) ) {
+			$email = $person->email_alt;
+		} else {
+			$email = $person->email;
+		}
+		if ( ! empty( $person->office_alt ) ) {
+			$office = $person->office_alt;
+		} else {
+			$office = $person->office;
+		}
+		if ( ! empty( $person->phone_alt ) ) {
+			$phone = $person->phone_alt;
+		} else {
+			$phone = $person->phone;
+		}
+		ob_start();
+		include( __DIR__ . '/templates/people.php' );
+		$html = ob_get_contents();
+		ob_end_clean();
+		return $html;
+	}
+
+	/**
+	 * AJAX profile request.
+	 */
+	public function profile_request() {
+		if ( $_POST['profile'] ) {
+			$response = wp_remote_get( 'https://people.wsu.edu/wp-json/posts/' . $_POST['profile'], array( 'sslverify' => false ) );
+			if ( is_wp_error( $response ) ) {
+				return '<!-- error -->';
+			}
+			$data = wp_remote_retrieve_body( $response );
+			if ( empty( $data ) ) {
+				return '<!-- error -->';
+			}
+			$person = json_decode( $data );
+			include( __DIR__ . '/templates/profile.php' );
+		}
+		exit;
+	}
+
+}
+
+new CAHNRSWP_People_Display();
